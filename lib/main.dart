29913c4bda4c98f857a8c91fc145a6a95f7a175d
sashitani ayumi <sasitani_611@yahoo.co.jp>
2020-05-30 16:02:52 +0900

    ボタンを二つ表示

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,39 @@ main() {
   ));
 }
 
+class PageTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
 class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("test"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('AnimatedContainer Sample'),
+    return Center(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        RaisedButton(
+          child: Text("テスト"),
           onPressed: () {
             Navigator.of(context).push(_createRoute());
           },
         ),
-      ),
+        RaisedButton(
+          child: Text("テスト2"),
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+        ),
+      ]),
     );
   }
 }
 
 Route _createRoute() {
   return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => AnimatedContainerSample(),
+    pageBuilder: (context, animation, secondaryAnimation) =>
+        AnimatedContainerSample(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       var begin = Offset(0.0, 1.0);
       var end = Offset.zero;
